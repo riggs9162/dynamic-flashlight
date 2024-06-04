40033b0bc277f67039f2549eb8f5a2c69f327410
Riggs <xconnorx@gmx.de>
2024-06-04 15:35:02 +0200

    overhaul

--- lua/autorun/sh_dynamic_flashlight.lua
+++ lua/autorun/sh_dynamic_flashlight.lua
@@ -1,46 +1,59 @@
-if CLIENT then
-    local cache = {}
-    local function UpdateCache(entity, state)
-        if not entity:IsPlayer() then return end
+CreateConVar("df_flashlight", 1, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Enable / Disable the dynamic flashlight system.")
+CreateClientConVar("df_flashlight_shadow", 1, true, true, "Enable / Disable the shadows of the dynamic flashlight system.")
+CreateClientConVar("df_flashlight_shadow_self", 1, true, true, "Enable / Disable the shadows of the dynamic flashlight system for yourself.")
 
-        if state then
-            table.insert(cache, entity)
-        else
-            for i = 1, #cache do
-                if cache[i] == entity then
-                    table.remove(cache, i)
-                end
-            end
+if ( CLIENT ) then
+    hook.Add("ShouldDrawShadows", "DynamicFlashlight.Shadows", function(entity)
+        if not ( GetConVar("df_flashlight"):GetBool() ) then return end
+
+        if ( entity == LocalPlayer() and GetConVar("df_flashlight_shadow"):GetBool() ) then
+            return GetConVar("df_flashlight_shadow_self"):GetBool()
         end
-    end
 
-    hook.Add("NotifyShouldTransmit", "DynamicFlashlight.PVS_Cache", function(entity, state)
-        UpdateCache(entity, state)
+        return GetConVar("df_flashlight_shadow"):GetBool()
     end)
 
-    hook.Add("EntityRemoved", "DynamicFlashlight.PVS_Cache", function(entity)
-        UpdateCache(entity, false)
-    end)
+    hook.Add("ShouldDrawFlashlight", "DynamicFlashlight.Flashlight", function(ply)
+        if not ( GetConVar("df_flashlight"):GetBool() ) then return end
 
+        return ply:GetNWBool("DynamicFlashlight")
+    end)
+    
     hook.Add("Think", "DynamicFlashlight.Rendering", function()
-        for i = 1, #cache do
-            local target = cache[i]
+        local ply = LocalPlayer()
 
-            if target:GetNWBool("DynamicFlashlight") then
-                if target.DynamicFlashlight then
-                    local position = target:GetPos()
+        for _, target in player.Iterator() do
+            if ( hook.Run("ShouldDrawFlashlight", target) == true ) then
+                if ( target.DynamicFlashlight ) then
+                    local pos = target:EyePos()
+                    local ang = target:EyeAngles()
 
-                    target.DynamicFlashlight:SetPos(Vector(position[1], position[2], position[3] + 40) + target:GetForward() * 20)
-                    target.DynamicFlashlight:SetAngles(target:EyeAngles())
+                    pos = pos + ang:Forward() * 32
+
+                    target.DynamicFlashlight:SetPos(pos)
+                    target.DynamicFlashlight:SetAngles(ang)
                     target.DynamicFlashlight:Update()
+
+                    local dlight = DynamicLight(target:EntIndex())
+                    if ( dlight ) then
+                        dlight.pos = pos
+                        dlight.r = 255
+                        dlight.g = 255
+                        dlight.b = 255
+                        dlight.brightness = 4
+                        dlight.Decay = 1000
+                        dlight.Size = 64
+                        dlight.DieTime = CurTime() + 1
+                    end
                 else
                     target.DynamicFlashlight = ProjectedTexture()
                     target.DynamicFlashlight:SetTexture("effects/flashlight001")
                     target.DynamicFlashlight:SetFarZ(900)
                     target.DynamicFlashlight:SetFOV(70)
+                    target.DynamicFlashlight:SetEnableShadows(hook.Run("ShouldDrawShadows", target))
                 end
             else
-                if target.DynamicFlashlight then
+                if ( target.DynamicFlashlight ) then
                     target.DynamicFlashlight:Remove()
                     target.DynamicFlashlight = nil
                 end
@@ -49,9 +62,49 @@ if CLIENT then
     end)
 else
     hook.Add("PlayerSwitchFlashlight", "DynamicFlashlight.Switch", function(ply, state)
-        ply:SetNWBool("DynamicFlashlight", not ply:GetNWBool("DynamicFlashlight"))
-        ply:EmitSound("items/flashlight1.wav", 60, 100)
+        if not ( GetConVar("df_flashlight"):GetBool() ) then return end
+
+        if not ( IsValid(ply) and ply:Alive() ) then return false end
+        if ( ply:InVehicle() ) then return false end
+        if ( ply:GetNoDraw() ) then return false end
+        if ( hook.Run("PlayerCanSwitchFlashlight", ply, state) == false ) then return false end
+
+        ply:SetNWBool("DynamicFlashlight", !ply:GetNWBool("DynamicFlashlight"))
+        ply:EmitSound("HL2Player.FlashLightOn")
 
         return false
     end)
+
+    hook.Add("PlayerSpawn", "DynamicFlashlight.Spawn", function(ply)
+        if not ( GetConVar("df_flashlight"):GetBool() ) then return end
+
+        ply:SetNWBool("DynamicFlashlight", false)
+    end)
+
+    hook.Add("PlayerEnteredVehicle", "DynamicFlashlight.Vehicle", function(ply, vehicle)
+        if not ( GetConVar("df_flashlight"):GetBool() ) then return end
+
+        ply:SetNWBool("DynamicFlashlight", false)
+    end)
+
+    hook.Add("PlayerNoClip", "DynamicFlashlight.NoClip", function(ply, state)
+        if not ( GetConVar("df_flashlight"):GetBool() ) then return end
+
+        timer.Simple(0.1, function() // comment: ensure that the player is actually in noclip
+            if ( IsValid(ply) and ply:GetMoveType() == MOVETYPE_NOCLIP and ply:GetNoDraw() ) then // comment: suppots gamemodes that make you invisible when noclip
+                ply:SetNWBool("DynamicFlashlight", false)
+            end
+        end)
+    end)
+end
+
+local PLAYER = FindMetaTable("Player")
+
+PLAYER.OldFlashlightIsOn = PLAYER.OldFlashlightIsOn or PLAYER.FlashlightIsOn
+function PLAYER:FlashlightIsOn()
+    if not ( GetConVar("df_flashlight"):GetBool() ) then
+        return self:OldFlashlightIsOn()
+    end
+
+    return self:GetNWBool("DynamicFlashlight")
 end
