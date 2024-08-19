aa04811584b707beceb22c98c14ff1ecb32d6091
Riggs <xconnorx@gmx.de>
2024-08-19 18:18:05 +0200

    Update sh_dynamic_flashlight.lua

--- lua/autorun/sh_dynamic_flashlight.lua
+++ lua/autorun/sh_dynamic_flashlight.lua
@@ -1,10 +1,14 @@
 CreateConVar("df_flashlight", 1, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Enable / Disable the dynamic flashlight system.")
 CreateClientConVar("df_flashlight_shadow", 1, true, true, "Enable / Disable the shadows of the dynamic flashlight system.")
 CreateClientConVar("df_flashlight_shadow_self", 1, true, true, "Enable / Disable the shadows of the dynamic flashlight system for yourself.")
+CreateClientConVar("df_flashlight_fov", 70, true, true, "Set the field of view of the dynamic flashlight system.")
+CreateClientConVar("df_flashlight_distance", 900, true, true, "Set the distance of the dynamic flashlight system.")
+CreateClientConVar("df_flashlight_texture", "effects/flashlight001", true, true, "Set the texture of the dynamic flashlight system.")
+CreateClientConVar("df_flashlight_sound", "HL2Player.FlashLightOn", true, true, "Set the sound of the dynamic flashlight system.")
 
 if ( CLIENT ) then
     hook.Add("ShouldDrawShadows", "DynamicFlashlight.Shadows", function(entity)
-        if not ( GetConVar("df_flashlight"):GetBool() ) then return end
+        if ( !GetConVar("df_flashlight"):GetBool() ) then return end
 
         if ( entity == LocalPlayer() and GetConVar("df_flashlight_shadow"):GetBool() ) then
             return GetConVar("df_flashlight_shadow_self"):GetBool()
@@ -14,7 +18,7 @@ if ( CLIENT ) then
     end)
 
     hook.Add("ShouldDrawFlashlight", "DynamicFlashlight.Flashlight", function(ply)
-        if not ( GetConVar("df_flashlight"):GetBool() ) then return end
+        if ( !GetConVar("df_flashlight"):GetBool() ) then return end
 
         return ply:GetNWBool("DynamicFlashlight")
     end)
@@ -22,35 +26,47 @@ if ( CLIENT ) then
     hook.Add("Think", "DynamicFlashlight.Rendering", function()
         local ply = LocalPlayer()
 
+        if ( !GetConVar("df_flashlight"):GetBool() ) then return end
+
+        local fov = GetConVar("df_flashlight_fov"):GetInt()
+        local distance = GetConVar("df_flashlight_distance"):GetInt()
+        local texture = GetConVar("df_flashlight_texture"):GetString()
+
         for _, target in player.Iterator() do
             if ( hook.Run("ShouldDrawFlashlight", target) == true ) then
                 if ( target.DynamicFlashlight ) then
-                    local pos = target:EyePos()
+                    local pos = target:GetShootPos() + target:GetAimVector() * 32
                     local ang = target:EyeAngles()
 
                     pos = pos + ang:Forward() * 32
 
+                    if ( !fov == target.DynamicFlashlightInfo.fov or !distance == target.DynamicFlashlightInfo.distance or !texture == target.DynamicFlashlightInfo.texture ) then
+                        target.DynamicFlashlight:SetTexture(texture)
+                        target.DynamicFlashlight:SetFarZ(distance)
+                        target.DynamicFlashlight:SetFOV(fov)
+
+                        target.DynamicFlashlightInfo.fov = fov
+                        target.DynamicFlashlightInfo.distance = distance
+                        target.DynamicFlashlightInfo.texture = texture
+                    end
+
                     target.DynamicFlashlight:SetPos(pos)
                     target.DynamicFlashlight:SetAngles(ang)
                     target.DynamicFlashlight:Update()
-
-                    local dlight = DynamicLight(target:EntIndex())
-                    if ( dlight ) then
-                        dlight.pos = pos
-                        dlight.r = 255
-                        dlight.g = 255
-                        dlight.b = 255
-                        dlight.brightness = 4
-                        dlight.Decay = 1000
-                        dlight.Size = 64
-                        dlight.DieTime = CurTime() + 1
-                    end
                 else
                     target.DynamicFlashlight = ProjectedTexture()
-                    target.DynamicFlashlight:SetTexture("effects/flashlight001")
-                    target.DynamicFlashlight:SetFarZ(900)
-                    target.DynamicFlashlight:SetFOV(70)
+                    target.DynamicFlashlight:SetTexture(texture)
+                    target.DynamicFlashlight:SetFarZ(distance)
+                    target.DynamicFlashlight:SetFOV(fov)
                     target.DynamicFlashlight:SetEnableShadows(hook.Run("ShouldDrawShadows", target))
+
+                    if ( !target.DynamicFlashlightInfo ) then
+                        target.DynamicFlashlightInfo = {}
+                    end
+
+                    target.DynamicFlashlightInfo.fov = fov
+                    target.DynamicFlashlightInfo.distance = distance
+                    target.DynamicFlashlightInfo.texture = texture
                 end
             else
                 if ( target.DynamicFlashlight ) then
@@ -62,9 +78,9 @@ if ( CLIENT ) then
     end)
 else
     hook.Add("PlayerSwitchFlashlight", "DynamicFlashlight.Switch", function(ply, state)
-        if not ( GetConVar("df_flashlight"):GetBool() ) then return end
+        if ( !GetConVar("df_flashlight"):GetBool() ) then return end
 
-        if not ( IsValid(ply) and ply:Alive() ) then return false end
+        if ( !IsValid(ply) or !ply:Alive() ) then return false end
         if ( ply:InVehicle() ) then return false end
         if ( ply:GetNoDraw() ) then return false end
         if ( hook.Run("PlayerCanSwitchFlashlight", ply, state) == false ) then return false end
@@ -76,19 +92,19 @@ else
     end)
 
     hook.Add("PlayerSpawn", "DynamicFlashlight.Spawn", function(ply)
-        if not ( GetConVar("df_flashlight"):GetBool() ) then return end
+        if ( !GetConVar("df_flashlight"):GetBool() ) then return end
 
         ply:SetNWBool("DynamicFlashlight", false)
     end)
 
     hook.Add("PlayerEnteredVehicle", "DynamicFlashlight.Vehicle", function(ply, vehicle)
-        if not ( GetConVar("df_flashlight"):GetBool() ) then return end
+        if ( !GetConVar("df_flashlight"):GetBool() ) then return end
 
         ply:SetNWBool("DynamicFlashlight", false)
     end)
 
     hook.Add("PlayerNoClip", "DynamicFlashlight.NoClip", function(ply, state)
-        if not ( GetConVar("df_flashlight"):GetBool() ) then return end
+        if ( !GetConVar("df_flashlight"):GetBool() ) then return end
 
         timer.Simple(0.1, function() // comment: ensure that the player is actually in noclip
             if ( IsValid(ply) and ply:GetMoveType() == MOVETYPE_NOCLIP and ply:GetNoDraw() ) then // comment: suppots gamemodes that make you invisible when noclip
@@ -102,9 +118,23 @@ local PLAYER = FindMetaTable("Player")
 
 PLAYER.OldFlashlightIsOn = PLAYER.OldFlashlightIsOn or PLAYER.FlashlightIsOn
 function PLAYER:FlashlightIsOn()
-    if not ( GetConVar("df_flashlight"):GetBool() ) then
+    if ( !GetConVar("df_flashlight"):GetBool() ) then
         return self:OldFlashlightIsOn()
     end
 
     return self:GetNWBool("DynamicFlashlight")
+end
+
+PLAYER.OldFlashlight = PLAYER.OldFlashlight or PLAYER.Flashlight
+function PLAYER:Flashlight(bool)
+    if ( !GetConVar("df_flashlight"):GetBool() ) then
+        if ( bool ) then
+            self:OldFlashlight(true)
+        else
+            self:OldFlashlight(false)
+        end
+    end
+
+    self:SetNWBool("DynamicFlashlight", bool)
+    self:EmitSound("HL2Player.FlashLightOn")
 end
